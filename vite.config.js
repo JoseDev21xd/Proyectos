beed323b33fcf22d51047dd6100babb0fd44f4cf
JoseDev21xd <155492576+JoseDev21xd@users.noreply.github.com>
2024-01-08 09:36:50 -0400

    Build files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'https://JoseDev21xd.github.io/Proyectos',
-})
+  base: "./",
+  build: {
+    outDir: "docs",
+  },
+});
